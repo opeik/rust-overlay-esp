a2a1410483eb987078ce611fe0ed3896a99d8420
Sandro Stikić <sandro@stikic.com>
2023-05-30 00:23:39 +0800

    Wip

--- flake.nix
+++ flake.nix
@@ -8,10 +8,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
-    embuild-src = {
-      flake = false;
-      url = "github:esp-rs/embuild/v0.31.2";
-    };
   };
 
   outputs = {
@@ -19,7 +15,6 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
-    embuild-src,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -29,33 +24,17 @@
           overlays = [(import rust-overlay) (import ./nix/overlay.nix)];
         };
       in {
-        # `nix build`
-        packages = {
-          ldproxy = pkgs.rustPlatform.buildRustPackage {
-            pname = "ldproxy";
-            version = "0.3.3";
-            src = embuild-src;
-            doCheck = false;
-            cargoBuildFlags = "--package ldproxy";
-            cargoHash = "sha256-2W5kKM0VV4C/L9dPxf4NDmi6VSleWvT27CfQbYCxo2U=";
-          };
-        };
         # `nix fmt`
         formatter = pkgs.alejandra;
-        # `nix develop`
 
+        # `nix develop`
         devShells.default = let
-          # rust-base = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          #   toolchain.minimal.override {
-          #     extensions = ["rust-src"];
-          #     targets = ["riscv32imc-unknown-none-elf" "riscv32imac-unknown-none-elf"];
-          #   });
           nix-tools = with pkgs; [nil self.formatter.${system}];
           rust-tools = with pkgs; [
-            rust-bin-esp32
+            #rust-bin-esp32
+            pkgs.ldproxy
             cargo-espflash
             cargo-espmonitor
-            self.packages.${system}.ldproxy
             (lib.optional stdenv.isDarwin libiconv)
           ];
         in
@@ -63,7 +42,6 @@
             buildInputs = [nix-tools rust-tools];
             shellHook = with pkgs; ''
               unset CC CXX
-              source ${rust-bin-esp32}/env.sh
             '';
           };
       }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,59 +1,67 @@
 final: prev: let
-  manifests = import ./manifests.nix;
-  system = final.system;
   pkgs = final.pkgs;
-
-  tarballs = builtins.listToAttrs (builtins.map
-    (file: {
-      name = file;
-      value = builtins.fetchurl {
-        url = manifests.${system}.${file}.url;
-        sha256 = manifests.${system}.${file}.sha256;
-      };
-    })
-    [
-      "rust-esp"
-      "rust-esp-src"
-      "llvm-esp"
-      "xtensa-esp32-elf"
-      "xtensa-esp32s2-elf"
-      "xtensa-esp32s3-elf"
-      "riscv32-esp-elf"
-    ]);
+  system = final.system;
 in {
-  rust-bin-esp32 = pkgs.stdenv.mkDerivation {
-    pname = "rust-bin-esp32";
-    version = "1.69.0.0";
-    dontUnpack = true;
-    buildPhase = ''
-      mkdir --parents $out tmp/{rust,rust-src} toolchains/esp/{\
-      xtensa-esp32-elf-clang/esp-15.0.0-20221201,\
-      xtensa-esp32s2-elf/esp-12.2.0_20230208,\
-      xtensa-esp32-elf/esp-12.2.0_20230208,\
-      xtensa-esp32s3-elf/esp-12.2.0_20230208,\
-      riscv32-esp-elf/esp-12.2.0_20230208}
+  rust-bin-esp32 = let
+    manifests = import ./manifests.nix;
+    bins = builtins.listToAttrs (builtins.map
+      (file: {
+        name = file;
+        value = builtins.fetchurl {
+          url = manifests.${system}.${file}.url;
+          sha256 = manifests.${system}.${file}.sha256;
+        };
+      })
+      (builtins.attrNames manifests.${system}));
+  in {
+    rust-bin-esp32 = pkgs.stdenv.mkDerivation {
+      pname = "rust-bin-esp32";
+      version = "1.69.0.0";
+      dontUnpack = true;
+      buildPhase = ''
+        mkdir --parents $out tmp/{rust,rust-src} esp/{\
+        xtensa-esp32-elf-clang/esp-15.0.0-20221201,\
+        xtensa-esp32s2-elf/esp-12.2.0_20230208,\
+        xtensa-esp32-elf/esp-12.2.0_20230208,\
+        xtensa-esp32s3-elf/esp-12.2.0_20230208,\
+        riscv32-esp-elf/esp-12.2.0_20230208}
 
-      tar --extract --file ${tarballs.rust-esp} --directory tmp/rust
-      tar --extract --file ${tarballs.rust-esp-src} --directory tmp/rust-src
-      tar --extract --file ${tarballs.llvm-esp} --directory toolchains/esp/xtensa-esp32-elf-clang/esp-15.0.0-20221201
-      tar --extract --file ${tarballs.xtensa-esp32-elf} --directory toolchains/esp/xtensa-esp32-elf/esp-12.2.0_20230208
-      tar --extract --file ${tarballs.xtensa-esp32s2-elf} --directory toolchains/esp/xtensa-esp32s2-elf/esp-12.2.0_20230208
-      tar --extract --file ${tarballs.xtensa-esp32s3-elf} --directory toolchains/esp/xtensa-esp32s3-elf/esp-12.2.0_20230208
-      tar --extract --file ${tarballs.riscv32-esp-elf} --directory toolchains/esp/riscv32-esp-elf/esp-12.2.0_20230208
+        tar --extract --file ${bins.rust-esp} --directory tmp/rust
+        tar --extract --file ${bins.rust-esp-src} --directory tmp/rust-src
+        tar --extract --file ${bins.llvm-esp} --directory esp/xtensa-esp32-elf-clang/esp-15.0.0-20221201
+        tar --extract --file ${bins.xtensa-esp32-elf} --directory esp/xtensa-esp32-elf/esp-12.2.0_20230208
+        tar --extract --file ${bins.xtensa-esp32s2-elf} --directory esp/xtensa-esp32s2-elf/esp-12.2.0_20230208
+        tar --extract --file ${bins.xtensa-esp32s3-elf} --directory esp/xtensa-esp32s3-elf/esp-12.2.0_20230208
+        tar --extract --file ${bins.riscv32-esp-elf} --directory esp/riscv32-esp-elf/esp-12.2.0_20230208
 
-      ./tmp/rust/rust-nightly-aarch64-apple-darwin/install.sh --destdir=toolchains/esp \
-        --prefix="" --without=rust-docs-json-preview,rust-docs --disable-ldconfig
-      ./tmp/rust-src/rust-src-nightly/install.sh --destdir=toolchains/esp --prefix="" --disable-ldconfig
-      mv toolchains $out
+        ./tmp/rust/rust-nightly-aarch64-apple-darwin/install.sh --destdir=esp \
+          --prefix="" --without=rust-docs-json-preview,rust-docs --disable-ldconfig
+        ./tmp/rust-src/rust-src-nightly/install.sh --destdir=esp --prefix="" --disable-ldconfig
+        mv esp $out
 
-      cat << 'EOF' > $out/env.sh
-      export LIBCLANG_PATH="$out/toolchains/esp/xtensa-esp32-elf-clang/esp-15.0.0-20221201/esp-clang/lib"
-      export PATH="$out/toolchains/esp/bin:$PATH"
-      export PATH="$out/toolchains/esp/xtensa-esp32s3-elf/esp-12.2.0_20230208/xtensa-esp32s3-elf/bin:$PATH"
-      export PATH="$out/toolchains/esp/riscv32-esp-elf/esp-12.2.0_20230208/riscv32-esp-elf/bin:$PATH"
-      export PATH="$out/toolchains/esp/xtensa-esp32s2-elf/esp-12.2.0_20230208/xtensa-esp32s2-elf/bin:$PATH"
-      export PATH="$out/toolchains/esp/xtensa-esp32-elf/esp-12.2.0_20230208/xtensa-esp32-elf/bin:$PATH"
-      EOF
-    '';
+        cat << 'EOF' > $out/env.sh
+        export LIBCLANG_PATH="$out/esp/xtensa-esp32-elf-clang/esp-15.0.0-20221201/esp-clang/lib"
+        export PATH="$out/esp/bin:$PATH"
+        export PATH="$out/esp/xtensa-esp32s3-elf/esp-12.2.0_20230208/xtensa-esp32s3-elf/bin:$PATH"
+        export PATH="$out/esp/riscv32-esp-elf/esp-12.2.0_20230208/riscv32-esp-elf/bin:$PATH"
+        export PATH="$out/esp/xtensa-esp32s2-elf/esp-12.2.0_20230208/xtensa-esp32s2-elf/bin:$PATH"
+        export PATH="$out/esp/xtensa-esp32-elf/esp-12.2.0_20230208/xtensa-esp32-elf/bin:$PATH"
+        EOF
+        substituteInPlace $out/env.sh --replace '$out' "$out"
+      '';
+    };
+  };
+
+  ldproxy = pkgs.rustPlatform.buildRustPackage {
+    pname = "ldproxy";
+    version = "0.3.3";
+    src = builtins.fetchGit {
+      url = "https://github.com/esp-rs/embuild.git";
+      ref = "refs/tags/v0.31.2";
+      rev = "2ec3bbc91cd3a4fda54e02c9f1bc6dc350a957b2";
+    };
+    doCheck = false;
+    cargoBuildFlags = "--package ldproxy";
+    cargoHash = "sha256-2W5kKM0VV4C/L9dPxf4NDmi6VSleWvT27CfQbYCxo2U=";
   };
 }
