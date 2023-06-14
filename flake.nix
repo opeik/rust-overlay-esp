39fdc24003921b36e401f3c17fc441389e2713d0
Sandro Stikić <sandro@stikic.com>
2023-06-14 23:38:54 +0800

    wip

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     rust-overlay,
     ...
   }:
-    flake-utils.lib.eachSystem ["aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-darwin"] (
+    flake-utils.lib.eachSystem ["aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux"] (
       system: let
         pkgs = import nixpkgs {
           inherit system;
